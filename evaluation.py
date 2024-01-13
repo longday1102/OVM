b2e5ac8ad13ae8cc1c3c1a1bcc06b75f704863f6
Hoàng Long <longhoangai1102@gmail.com>
2024-01-13 23:10:07 +0700

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -59,7 +59,6 @@ if __name__ == "__main__":
             backbone = generator,
             checkpoint_dir = args.verifier_weight_path,
         )
-        print(verifier)
         infer = ValueGuideBeamSearch(
             verifier = verifier,
             tokenizer = tokenizer
